51668f6cf59b433e0ff41ddb5072275e40ff7137
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-11-29 14:28:49 +0700

    feat(user): fix register and add update delete

--- delivery/controller/user_controller.go
+++ delivery/controller/user_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"task-5-pbi-btpns-achmad-dinofaldi-firmansyah/delivery/middleware"
 	"task-5-pbi-btpns-achmad-dinofaldi-firmansyah/model"
 	"task-5-pbi-btpns-achmad-dinofaldi-firmansyah/usecase"
 
@@ -24,7 +25,7 @@ func (u *UserController) Register(c *gin.Context) {
 		return
 	}
 
-	user, err = u.userUseCase.Register(user)
+	err = u.userUseCase.Register(user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status":  "failed",
@@ -35,7 +36,97 @@ func (u *UserController) Register(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{
 		"status": "success",
-		"data":   user,
+	})
+}
+
+func (u *UserController) Login(c *gin.Context) {
+	var userCred model.UserCredential
+	err := c.ShouldBindJSON(&userCred)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	token, err := u.userUseCase.Login(userCred)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "success",
+		"token":  token,
+	})
+}
+
+func (u *UserController) Update(c *gin.Context) {
+	var user model.User
+	id := c.Param("id")
+
+	_, err := u.userUseCase.FindById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  "failed",
+			"message": "user not found",
+		})
+		return
+	}
+
+	user.Id = id
+	err = c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	err = u.userUseCase.Update(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "success",
+	})
+}
+
+func (u *UserController) Delete(c *gin.Context) {
+	var user model.User
+	id := c.Param("id")
+
+	_, err := u.userUseCase.FindById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  "failed",
+			"message": "user not found",
+		})
+		return
+	}
+
+	user.Id = id
+	err = u.userUseCase.Delete(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "failed",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "success",
 	})
 }
 
@@ -47,4 +138,7 @@ func NewUserController(router *gin.Engine, userUseCase usecase.UserUseCase) {
 
 	routerGroup := router.Group("/users")
 	routerGroup.POST("/register", controller.Register)
+	routerGroup.POST("/login", controller.Login)
+	routerGroup.PUT("/:id", middleware.AuthMiddleware(), controller.Update)
+	routerGroup.DELETE("/:id", middleware.AuthMiddleware(), controller.Delete)
 }
