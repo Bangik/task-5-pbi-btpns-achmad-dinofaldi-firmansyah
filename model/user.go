a92d09dd8bf4781f52fc7f871e9c007b7e721a17
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-11-30 10:05:42 +0700

    refac(user): add new struct to user model

--- model/user.go
+++ model/user.go
@@ -3,15 +3,21 @@ package model
 import "time"
 
 type User struct {
-	Id        string `json:"id" gorm:"primaryKey"`
-	Username  string `json:"username" binding:"required,max=255"`
-	Email     string `json:"email" binding:"required,max=255" gorm:"unique"`
-	Password  string
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Id       string `json:"id" gorm:"primaryKey"`
+	Username string `json:"username" binding:"required,max=255"`
+	Email    string `json:"email" binding:"required,max=255" gorm:"unique"`
+	Password string `json:"password" binding:"required,min=6,max=255"`
 }
 
 type UserCredential struct {
 	Email    string `json:"email" binding:"required,max=255"`
 	Password string `json:"password" binding:"required,min=6,max=255"`
+}
+
+type UserResponse struct {
+	Id        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
